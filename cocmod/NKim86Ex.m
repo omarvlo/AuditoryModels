61f5f06864b3e02a908c4a7c827a4cc8d4231482
omarvlo <69394469+omarvlo@users.noreply.github.com>
2022-08-11 16:03:57 -0500

    Add files via upload

---
+++ cocmod/NKim86Ex.m
@@ -0,0 +1,183 @@
+% Neely and Kim's (1986) cochlear model  
+% Computes Pressures and Velocities 
+% Ref: S. Neely, D. Kim, "A Model for Active Elements in Cochlear Biomechanics",  
+%J. Acoust. Soc. Am. 79(5), pp. 1472-1480, May 1986. 
+ 
+ 
+clear 
+clc 
+ 
+disp(' Neely and Kim?s model (1986)'); 
+disp(' Impedance with Nf=220 freq. points between 100 Hz and 50kHz and Nx=500 sections.'); 
+ 
+Nf=220; 
+Nx=500; 
+L=2.5;		%--- cochlear length 
+ 
+f=logspace(2,log10(50000),Nf)'; 
+x=linspace(0,L,Nx); 
+s=j*2*pi*f; 
+ 
+%[Zp,Zpmod,fQ,Mef]=NKim86Z(f,x);	%--- default gama=1 
+%fZa=fQ(1,:);fHz=fQ(2,:);fHp=fQ(3,:); 
+%QZa=fQ(4,:);QHz=fQ(5,:);QHp=fQ(6,:); 
+%plot(x,log(fZa),x,log(fHz),x,log(fHp)) 
+ 
+%or simply: Zp=NKim86Z(f,x); 
+%-------------------------- 
+ 
+%--- discrete computations ------ 
+%================================ 
+ 
+gama=1; 
+g=1; 
+b=0.4; 
+k1=1.1e9*exp(-4*x); 
+c1=20+1500*exp(-2*x); 
+m1=3e-3; 
+k2=7e6*exp(-4.4*x); 
+c2=10*exp(-2.2*x); 
+m2=0.5e-3; 
+k3=1e7*exp(-4*x); 
+c3=2*exp(-0.8*x); 
+k4=6.15e8*exp(-4*x); 
+c4=1040*exp(-2*x); 
+
+% figure(1)
+% subplot(5,1,1)
+% plot(x,k1)
+% title('Parámetro k1'), xlabel('x'), ylabel('k1')
+% subplot(5,1,2)
+% plot(x,c1)
+% title('Parámetro c1'), xlabel('x'), ylabel('c1')
+% subplot(5,1,3)
+% plot(x,m1)
+% title('Parámetro m1'), xlabel('x'), ylabel('m1')
+% subplot(5,1,4)
+% plot(x,k2)
+% title('Parámetro k2'), xlabel('x'), ylabel('k2')
+% subplot(5,1,5)
+% plot(x,c2)
+% title('Parámetro c2'), xlabel('x'), ylabel('c2')
+
+% figure(2)
+% subplot(5,1,1)
+% plot(x,m2)
+% title('Parámetro m2'), xlabel('x'), ylabel('m2')
+% subplot(5,1,2)
+% plot(x,k3)
+% title('Parámetro k3'), xlabel('x'), ylabel('k3')
+% subplot(5,1,3)
+% plot(x,c3)
+% title('Parámetro c3'), xlabel('x'), ylabel('c3')
+% subplot(5,1,4)
+% plot(x,k4)
+% title('Parámetro k4'), xlabel('x'), ylabel('k4')
+% subplot(5,1,5)
+% plot(x,c4)
+% title('Parámetro c4'), xlabel('x'), ylabel('c4')
+
+invs=1 ./s; 
+umf=ones(Nf,1); 
+umx=ones(1,Nx); 
+ 
+Z1=invs*k1 + c1(umf,:) + m1*s(:,umx);   %--- Z1=k1/s+c1+s*m1; 
+Z2=invs*k2 + c2(umf,:) + m2*s(:,umx);	%--- Z2=k2/s+c2+s*m2; 
+Z3=invs*k3 + c3(umf,:);			%--- Z3=k3/s+c3; 
+Z4=invs*k4 + c4(umf,:);			%--- Z4=k4/s+c4; 
+Hc=Z2./(Z2+Z3); 
+ 
+Zp=(g/b)*( Z1 + Hc.*(Z3-gama*Z4) ); 
+ 
+%---------ZBM=(g/b)*Z1; 
+%---------ZOC=(g/b)*Hc.*(Z3-gama*Z4); 
+ 
+ 
+ 
+%====================================================================== 
+ 
+disp(' Middle-ear connection') 
+ 
+%-------- middle ear ------------- 
+km=2.1e5; 
+cm=400; 
+mm=45e-3; 
+As=0.01; 
+Am=0.35; 
+gm=0.5; 
+Zm = s*mm + cm + km./s; 
+Pi = Am/gm/As; 
+%--------------------------------- 
+ 
+ 
+disp(' Transmission Line Model') 
+ 
+Dx=L/Nx; 
+h=0.1; 
+rho=1; 
+zs=2*s*rho*Dx; 
+zp=h*Zp/Dx; 
+zp(:,Nx)=sqrt(2*s*rho*h.*Zp(:,Nx))-zs; 
+ 
+[P,Ps,Zin]=TLineME(Pi,Zm,zs,zp); 
+VBM = -P./Zp; 
+VC=Hc.*VBM; 
+ 
+
+figure(4)
+ 
+disp(' Ploting Responses every 100 sections...') 
+ 
+semilogx(f,db(P(:,1:100:Nx),1e-5)) 
+title('|P(f)| (pressure)'), xlabel('f [Hz]'), ylabel('dB')%, pause 
+% semilogx(f,fase(P(:,1:100:Nx))) 
+% title('arg{P(f)}'), xlabel('f [Hz]'), ylabel('x pi'), pause 
+%  
+%  
+%  
+% semilogx(f,db(VBM(:,1:100:Nx))) 
+% title('|VBM(f)| (BM velocity)'), xlabel('f [Hz]'), ylabel('dB'), pause 
+% semilogx(f,fase(VBM(:,1:100:Nx))) 
+% title('arg{VBM(f)}'), xlabel('f [Hz]'), ylabel('x pi'), pause 
+%  
+% semilogx(f,db(VC(:,1:100:Nx))) 
+% title('|VC(f)| (cilia velocity)'), xlabel('f [Hz]'), ylabel('dB'), pause 
+% semilogx(f,fase(VC(:,1:100:Nx))) 
+% title('arg{VC(f)}'), xlabel('f [Hz]'), ylabel('x pi'), pause 
+%  
+%  
+% %---------------POWER------------------------ 
+% clc 
+% disp(' Ratio of power absorbed by partition and input power for 2, 4 and 8 kHz') 
+%  
+%  
+% Potin=real(Ps.*conj(Ps./Zin))/2; 
+% PotBM =real(P.*conj(P./zp))/2; 
+%  
+% for k=[155,131,107]; 
+%  plot(x/L,PotBM(k,:)/Potin(k)),hold on 
+% end 
+% hold off,  xlabel('x/L'),ylabel('dB'),title('Pot/Potin') 
+%  
+% %======================================================= 
+f_round = f;
+
+P_abs = db(P);
+for i=1:1:Nx
+vector_p = P_abs(:,i);
+[M,maximo] = max(vector_p);
+distancia = i;
+frecuencia(i) = f(maximo);
+a = 1;
+end
+
+f_axis = zeros(1,50e3);
+
+figure(5)
+plot(x,frecuencia)
+title('Relación frecuencia-distancia'), xlabel('mm a partir de la ventana oval'), ylabel('Frecuencia (Hz)'),
+
+figure(6)
+plot(frecuencia,x)
+title('Relación distancia-frecuencia'), xlabel('Frecuencia (Hz)'), ylabel('mm a partir de la ventana oval'),
+
